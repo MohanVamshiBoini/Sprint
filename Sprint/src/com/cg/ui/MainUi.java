13679457c69ca824c1ac9a1d9a328564e7c55c2c
MohanVamshiBoini <62380864+MohanVamshiBoini@users.noreply.github.com>
2020-03-21 15:00:13 +0530

    A to option and B to run

--- Sprint/src/com/cg/ui/MainUi.java
+++ Sprint/src/com/cg/ui/MainUi.java
@@ -17,8 +17,8 @@ public class MainUi {
 		String pwd = "";
 		String centreName = "";
 		Scanner s = new Scanner(System.in);
-		int a = 0;
-		boolean b = false;
+		int option = 0;
+		boolean run = false;
 		while (true) {
 			System.out.println("Enter Login Id");// Admin Login
 			loginId = s.next();
@@ -26,18 +26,18 @@ public class MainUi {
 			pwd = s.next();
 			if ((loginId.equals("vk18") && pwd.equals("vk18")) || (loginId.equals("manutd") && pwd.equals("manutd123"))
 					|| (loginId.equals("cricket") && pwd.equals("virat"))) {
-				b = true;
+				run = true;
 			} else {
 				System.out.println("Enter Valid login details");
 			}
-			while (b) { // Options For Admin
+			while (run) { // Options For Admin
 				try {
 					System.out.println("1.Add Diagnostic Centre"); // To add new center into availability
 					System.out.println("2.Remove Diagnostic Centre");// To remove center from availability
 					System.out.println("3.Exit"); // To exit out of menu
-					a = s.nextInt();
-					if (a == 1 || a == 2 || a == 3) {
-						switch (a) {
+					option = s.nextInt();
+					if (option == 1 || option == 2 || option == 3) {
+						switch (option) {
 						case 1:
 							while (true) {
 								System.out.println("Enter Diagnostic Centre name which is not present in list");
